e787d7cb71f731fc8958f7cd252baa8654c3d7ab
aaron-su98 <mr.aaron.su@gmail.com>
2023-02-28 11:45:49 -0500

    Create OrderItems model, update index.js association and exports

--- server/db/index.js
+++ server/db/index.js
@@ -1,23 +1,25 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
-const Product = require('./models/Product')
-const Order = require('./models/Order')
+const User = require("./models/User");
+const Product = require("./models/Product");
+const Order = require("./models/Order");
+const OrderItems = require("./models/OrderItems");
 
 //associations could go here!
 User.hasMany(Order);
 Order.belongsTo(User);
 
-Product.belongsToMany(Order, {through: "Order_Items"})
-Order.hasMany(Product)
+Product.belongsToMany(Order, { through: OrderItems });
+Order.hasMany(Product);
 
 module.exports = {
   db,
   models: {
     User,
     Product,
-    Order
+    Order,
+    OrderItems,
   },
-}
+};
