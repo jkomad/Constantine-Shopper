09c2d7c67683508a0666dd72fd61efce9c01e053
Erik Tonnesen <estonnesen@gmail.com>
2023-03-01 10:01:18 -0500

    started addProduct component

---
+++ client/features/products/AddProduct.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+const AddProduct = ({ AddProduct }) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [gpa, setGpa] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    addStudent(name, price, description, imgUrl);
+
+    setName("");
+    setPrice("");
+    setDescription("");
+    setImgUrl("");
+  };
+
+  return (
+    <form className="formBox" onSubmit={handleSubmit}>
+        <h3 className="addNew">Add New Product</h3>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        placeholder="Product Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      {/* <label htmlFor="name">Last Name:</label> */}
+      <input
+        type="text"
+        id="price"
+        value={price}
+        placeholder="Price"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      {/* <label>description</label> */}
+      <input
+        type="text"
+        id="description"
+        value={description}
+        placeholder="description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      {/* <label>imgUrl</label> */}
+      <input
+        type="text"
+        id="imgUrl"
+        value={imgUrl}
+        placeholder="Image URL"
+        onChange={(e) => setImgUrl(e.target.value)}
+      />
+      <button className="deleteButton" type="submit">SUBMIT</button>
+    </form>
+  );
+};
+
+export default AddProduct;
