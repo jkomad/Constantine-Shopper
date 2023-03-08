6fa4a10458a74fa52da539eea0a6ff413c9830a5
Sandrieli13 <tskitishvilisandro@gmail.com>
2023-03-08 09:35:55 -0500

    Single Product
    Hi

--- client/features/productsBeta/ProductMeta.js
+++ client/features/productsBeta/ProductMeta.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import { ProductMetaWrapper } from "../../styles/product";
+import { Link } from "react-router-dom";
 export default function ProductMeta({ product, matches }) {
     return (
       <ProductMetaWrapper>
         <Typography variant={matches ? "h6" : "h5"} lineHeight={2}>
-          {product.name}
+          <Link style={{ textDecoration: 'none', color: 'inherit', fontFamily: 'sans-serif' }} to={`/products/${product.id}`}>{product.name}</Link>
         </Typography>
         <Typography variant={matches ? "caption" : "body1"}>
           ${product.price}
